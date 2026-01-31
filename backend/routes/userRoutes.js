50f44e54626c93c38850e5cf647955b6a8ad6ee5
avirat <aviratanand2202@gmail.com>
2026-01-31 16:39:51 +0530

    Add persistent JSON-based user storage

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+const USERS_FILE = path.join(__dirname, "../data/users.json");
+
+function readUsers() {
+  const data = fs.readFileSync(USERS_FILE, "utf-8");
+  return JSON.parse(data);
+}
+
+function writeUsers(users) {
+  fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
+}
+
 const express = require("express");
 const router = express.Router();
-
-const { users } = require("../models/User");
 
 // test route
 router.get("/test", (req, res) => {
@@ -16,17 +28,23 @@ router.post("/signup", (req, res) => {
     return res.status(400).json({ error: "Name and email required" });
   }
 
-  users.push({ name, email });
+  const users = readUsers();   // read from JSON file
+
+  users.push({ name, email }); // add new user
+
+  writeUsers(users);           // save back to JSON file
 
   res.json({
-    message: "User added successfully",
-    users
+    message: "User added successfully"
   });
 });
 
+
 // get all users
 router.get("/users", (req, res) => {
+  const users = readUsers();   // read from JSON file
   res.json(users);
 });
+
 
 module.exports = router;
